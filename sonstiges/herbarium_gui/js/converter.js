8abe1f22eaf06e5672c625d124f42d7dec2edee8
Marcel O <ortega_marcel@yahoo.de>
2018-07-05 15:26:47 +0200

    GUI zum Hinzufügen von Ingredienzen erstellt

---
+++ sonstiges/herbarium_gui/js/converter.js
@@ -0,0 +1,122 @@
+// Polyfills
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+if (!String.prototype.toLatexLabel) {
+  String.prototype.toLatexLabel = function() {
+    var args = arguments;
+    return this.toLowerCase().replace(/ä|ö|ü|ß/g, function(match, number) { 
+      if (match === "ä") return "ae";
+      else if (match === "ö") return "oe";
+      else if (match === "ü") return "ue";
+      else if (match === "ß") return "ss";
+    });
+  };
+}
+
+docReady(function() {
+  var Name = document.getElementById("name");
+  var Beschreibung = document.getElementById("beschreibung");
+  var Hauptwirkstoff = document.getElementById("hwirkstoff");
+  var Sekundärwirkstoff = document.getElementById("swirkstoff");
+  var Farbe = document.getElementById("farbe");
+  var Geruch = document.getElementById("geruch");
+  var Geschmack = document.getElementById("geschmack");
+  var Spezialeigenschaften = document.getElementById("spezialeigenschaften");
+  var Probe = document.getElementById("probe");
+  var Menge = document.getElementById("menge");
+  var Vorkommen = document.getElementById("vorkommen");
+  var Region = document.getElementById("region");
+  var Wert = document.getElementById("wert");
+  var Utensilien = document.getElementById("utensilien");
+  var Latex = document.getElementById("latex");
+
+  window.resetAll = function() {
+    Name.value = "";
+    Beschreibung.value = "";
+    Hauptwirkstoff.value = "";
+    Sekundärwirkstoff.value = "";
+    Farbe.value = "";
+    Geruch.value = "";
+    Geschmack.value = "";
+    Spezialeigenschaften.value = "";
+    Probe.value = "";
+    Menge.value = "1 pro Pflanze";
+    Vorkommen.value = "";
+    Region.value = "überall";
+    Wert.value = "";
+    Utensilien.value = "";
+    Latex.value = "";
+  }
+
+  window.convertTable = function() {
+    if (Name.value) {
+      Latex.value = getTable();
+    } else {
+      Latex.value = getTable();
+      // alert("Bitte gebe den Namen der Ingredienz an.")
+    }
+  }
+
+  window.convertAll = function() {
+    if (Name.value) {
+      Latex.value = getAll();
+    } else {
+      Latex.value = getAll();
+      // alert("Bitte gebe den Namen der Ingredienz an.")
+    }
+  }
+
+  function getAll() {
+    var latexStr = "\\subsection{{0}}\n{1}\n\n".format(
+      Name.value, 
+      Beschreibung.value || "\\textit{Keine Beschreibung vorhanden.}"
+    );
+
+    return latexStr + getTable();
+  }
+
+  function getTable() {
+    var tableStr = "\\begin{table}[h] \n\
+\\begin{center} \n\
+\\begin{tabular}{|l|l|p{1cm}|l|l|} \n\
+  \t\\cline{1-2} \cline{4-5} \n\
+  \t\\textbf{Hauptwirkstoff} & {0} && \\textbf{Pflückprobe} & {6} \\\\ \\cline{1-2} \\cline{4-5} \n\
+  \t\\textbf{Sekundärwirkstoff} & {1} && \\textbf{Menge} & {7} \\\\ \\cline{1-2} \\cline{4-5} \n\
+  \t\\textbf{Farbe} & {2} && \\textbf{Vorkommen} & {8} \\\\ \\cline{1-2} \\cline{4-5} \n\
+  \t\\textbf{Geruch} & {3} && \\textbf{Region} & {9} \\\\ \\cline{1-2} \\cline{4-5} \n\
+  \t\\textbf{Geschmack} & {4} && \\textbf{Wert} & {10} \\\\ \\cline{1-2} \\cline{4-5} \n\
+  \t\\textbf{Spezialeigenschaften} & {5} && \\textbf{Utensilien} & {11} \\\\ \\cline{1-2} \\cline{4-5} \n\
+\\end{tabular} \n\
+\\end{center} \n\
+\\caption{} \n\
+\\label{tab:{12}} \n\
+\\end{table}";
+
+      return tableStr.format(
+        Hauptwirkstoff.value || "-",
+        Sekundärwirkstoff.value || "-",
+        Farbe.value || "-",
+        Geruch.value || "-",
+        Geschmack.value || "-",
+        Spezialeigenschaften.value || "-",
+        Probe.value || "-",
+        Menge.value || "1 pro Pflanze",
+        Vorkommen.value || "-",
+        Region.value || "überall",
+        Wert.value ? Wert.value + "Kr" : "n.a.",
+        Utensilien.value || "-",
+        Name.value.toLatexLabel() || "-"
+      );
+  }
+});
